8addc8321b227ab3453acd19d9919268952ca9cb
sekine <sekine@cloud-ace.jp>
2019-04-26 03:25:33 +0900

    reflect statemachine diagram to model

--- domain/task.go
+++ domain/task.go
@@ -5,4 +5,77 @@ type Task struct {
 	Title       string
 	Description string
 	Assignee    string
+	State       State
+}
+
+func NewTask(title, description string) Task {
+	return Task{
+		// TODO: assign unique ID
+		Id:          "1000",
+		Title:       title,
+		Description: description,
+		Assignee:    "Bob",
+		State:       StateTodo{},
+	}
+}
+
+func (t Task) Assign(user string) Task {
+	t.Assignee = user
+	t.State = StateAssigned{}
+	return t
+}
+
+func (t Task) UnAssign() Task {
+	t.Assignee = ""
+	t.State = StateTodo{}
+	return t
+}
+
+func (t Task) Close() Task {
+	t.State = StateClosed{}
+	return t
+}
+
+func (t Task) CurrentState() string {
+	return t.State.CurrentState()
+}
+
+type State interface {
+	CurrentState() string
+}
+
+type StateTodo struct{}
+
+func (s StateTodo) CurrentState() string {
+	return "TODO"
+}
+
+type StateAssigned struct{}
+
+func (s StateAssigned) CurrentState() string {
+	return "ASSIGNED"
+}
+
+type StateDoing struct{}
+
+func (s StateDoing) CurrentState() string {
+	return "DOING"
+}
+
+type StatePaused struct{}
+
+func (s StatePaused) CurrentState() string {
+	return "PAUSED"
+}
+
+type StateCompleted struct{}
+
+func (s StateCompleted) CurrentState() string {
+	return "COMPLETED"
+}
+
+type StateClosed struct{}
+
+func (s StateClosed) CurrentState() string {
+	return "CLOSED"
 }
--- domain/task_test.go
+++ domain/task_test.go
@@ -1,1 +1,22 @@
 package domain
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestChangeTaskState(t *testing.T) {
+	task := NewTask("title", "description")
+	assert.Equal(t, "TODO", task.CurrentState())
+
+	task = task.Assign("John")
+	assert.Equal(t, "ASSIGNED", task.CurrentState())
+	assert.Equal(t, "John", task.Assignee)
+
+	task = task.UnAssign()
+	assert.Equal(t, "TODO", task.CurrentState())
+	assert.Equal(t, "", task.Assignee)
+
+	task = task.Close()
+	assert.Equal(t, "CLOSED", task.CurrentState())
+}
